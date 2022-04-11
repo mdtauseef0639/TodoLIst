824edf02a7acd3ddf18b5068efe196340e2c680f
Md Tauseef Kamal <t.kamal@axelor.com>
2022-04-11 12:13:49 +0530

    version 2

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,49 +1,94 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import Todolist from './Todolist';
-import Button from '@mui/material/Button';
+import Todolist from "./Todolist";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
 
 export default function App() {
-  const [inputText,setInputText] = useState("")
-  const [item,setItem] = useState([
-    
-  ])
-  function handleChange(e)
-  {
-    setInputText(e.target.value)
+  const [inputText, setInputText] = useState("");
+  const [item, setItem] = useState(JSON.parse(localStorage.getItem("list")));
+
+  const [toggleEdit, setToggleEdit] = useState(false);
+  const [isEditItem, setIsEditItem] = useState(null);
+  function handleChange(e) {
+    setInputText(e.target.value);
   }
 
-  function handleClick()
-  {
-    setItem(preValue=>{
-      return[...preValue,inputText]
+  function handleClick() {
+    if (!inputText) {
+      alert("add item first....");
+    } else if (inputText && toggleEdit) {
+      setItem(
+        item.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...elem, value: inputText };
+          }
+          return elem;
+        })
+      );
+      setToggleEdit(false);
+      setInputText("");
+      setIsEditItem(null);
+    } else {
+      const listItem = {
+        id: new Date().getTime().toString(),
+        value: inputText,
+      };
+      setItem((preValue) => {
+        return [...preValue, listItem];
+      });
+      setInputText(" ");
+    }
+  }
+
+  function deleteItem(id) {
+    setItem((prevItem) => {
+      return prevItem.filter((item) => {
+        return item.id !== id;
+      });
     });
   }
 
-  function deleteItem(id){
-    setItem(prevItem=>{
-      return prevItem.filter((item,index)=>{
-        return index!==id;
-      });
-    })
-  }
-  console.log(item)
+  function editItem(id) {
+    let newEditItem = item.find((elem) => {
+      return elem.id === id;
+    });
 
+    setInputText(newEditItem.value);
+    setIsEditItem(id);
+    setToggleEdit("true");
+  }
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(item));
+  }, [item]);
 
   return (
-    <div className='container'>
-      
-       <input type="text" value={inputText} onChange={handleChange}/>
-       <Button onClick={handleClick} variant="contained">Contained</Button>
-       <ul>{
-         item.map((item,index)=>{
-            return(<Todolist key ={index} id={index} text={item} onChecked={deleteItem}/>)
-         })
-       }
-       </ul>
-       
-        
-      
+    <div className="container">
+      <input type="text" value={inputText} onChange={handleChange} />
+      {toggleEdit ? (
+        <Button onClick={handleClick}>
+          <EditIcon />
+        </Button>
+      ) : (
+        <Button onClick={handleClick} variant="contained">
+          Add
+        </Button>
+      )}
+
+      <ul>
+        {item.map((item) => {
+          console.log(item);
+          return (
+            <Todolist
+              key={item.id}
+              id={item.id}
+              text={item.value}
+              onChecked={deleteItem}
+              onEdit={editItem}
+            />
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 export default function Todolist(props) {
   return (
    <div>
-       <li>{ props.text } <Button variant="text" onClick={()=>{
+       <li>{props.text}<Button variant='text' onClick={()=>{
+         props.onEdit(props.id)
+       }}><EditIcon/></Button><Button variant="text" onClick={()=>{
            props.onChecked(props.id)
        }}><DeleteIcon/></Button></li>
 
