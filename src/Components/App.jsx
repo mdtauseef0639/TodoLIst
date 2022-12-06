2b5d28cb319058cc66a1fcc9918ac560f25dd7dd
mdtauseef0639 <md.tauseef.kamal.0639@gmail.com>
2022-12-07 03:10:31 +0530

    Added backend support

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,72 +2,140 @@ import React, { useEffect, useState } from "react";
 
 import Todolist from "./Todolist";
 import Button from "@mui/material/Button";
-import EditIcon from "@mui/icons-material/Edit";
+import Box from '@mui/material/Box';
 
 export default function App() {
-  const [inputText, setInputText] = useState("");
-  const [item, setItem] = useState(JSON.parse(localStorage.getItem("list")));
+  const [inputTask, setInputTask] = useState("");
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputTime, setInputTime] = useState("");
+
+  const [item, setItem] = useState();
 
   const [toggleEdit, setToggleEdit] = useState(false);
   const [isEditItem, setIsEditItem] = useState(null);
+
+
   function handleChange(e) {
-    setInputText(e.target.value);
+    setInputTask(e.target.value);
   }
 
-  function handleClick() {
-    if (!inputText) {
+  function handleTimeChange(e) {
+    setInputTime(e.target.value);
+  }
+
+  function handleTitleChange(e) {
+    setInputTitle(e.target.value);
+  }
+
+  const handleClick = async e=> {
+    if (!inputTask) {
       alert("add item first....");
-    } else if (inputText && toggleEdit) {
-      setItem(
-        item.map((elem) => {
-          if (elem.id === isEditItem) {
-            return { ...elem, value: inputText };
-          }
-          return elem;
-        })
-      );
+    } else if (inputTask && toggleEdit) {
+      const body = {title:inputTitle,task:inputTask,time:inputTime}
+      try{
+        const response = await fetch(`http://localhost:8000/todos/${isEditItem}`,{
+          method:"PUT",
+          headers:{"Content-Type":"application/json",'Access-Control-Allow-Origin':'*'},
+          body:JSON.stringify(body),
+          mode:"cors"
+        });
+        console.log(response);
+      }
+      catch(e){
+
+      }
+      
       setToggleEdit(false);
-      setInputText("");
+      setInputTask("");
+      setInputTime("");
+      setInputTitle("");
+
+
       setIsEditItem(null);
     } else {
       const listItem = {
-        id: new Date().getTime().toString(),
-        value: inputText,
+        todo_id: new Date().getTime().toString(),
+        task: inputTask,
+        time:inputTime,
+        title:inputTitle
       };
-      setItem((preValue) => {
-        return [...preValue, listItem];
-      });
-      setInputText(" ");
+
+      try{
+          const body ={todo_id:listItem.todo_id,title:listItem.title,task:listItem.task,time:listItem.time}
+          const response = await fetch("http://localhost:8000/todos",{
+            method:"POST",
+            headers:{"Content-Type":"application/json",'Access-Control-Allow-Origin':'*'},
+            body:JSON.stringify(body),
+            mode:"cors"
+          });
+          console.log(response);
+      }
+      catch(e){
+
+      }
+      setInputTask(" ");
+      setInputTime("");
+      setInputTitle("");
     }
   }
 
-  function deleteItem(id) {
+  const deleteItem = async (id)=> {
+
+    try{
+      const response = await fetch(`http://localhost:8000/todos/${id}`,{
+        method:"DELETE"
+      });
+      console.log(response);
+    }
+    catch(e){
+
+    }
+
     setItem((prevItem) => {
       return prevItem.filter((item) => {
-        return item.id !== id;
+        return item.todo_id !== id;
       });
     });
   }
 
   function editItem(id) {
     let newEditItem = item.find((elem) => {
-      return elem.id === id;
+      return elem.todo_id === id;
     });
 
-    setInputText(newEditItem.value);
+    setInputTask(newEditItem.task);
     setIsEditItem(id);
     setToggleEdit("true");
+    setInputTime(newEditItem.time);
+    setInputTitle(newEditItem.title);
+      
   }
+
+const getTodos = async () => {
+  try{
+    const response =await fetch("http://localhost:8000/todos");
+    const jsonData = await response.json();
+    setItem(jsonData);
+  }
+  catch(err){
+    console.log(err.message);
+  }
+}
+
   useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(item));
-  }, [item]);
+    
+    getTodos();
+    
+  },[item]);
 
   return (
     <div className="container">
-      <input type="text" value={inputText} onChange={handleChange} />
+      <input type="text" value={inputTitle} onChange={handleTitleChange} placeholder="Enter Title" />
+      <input type="text" value={inputTask} onChange={handleChange} placeholder="Enter Task" />
+      <input type="time" value={inputTime} onChange={handleTimeChange}  />
       {toggleEdit ? (
-        <Button onClick={handleClick}>
-          <EditIcon />
+        <Button onClick={handleClick} variant="contained">
+          Save
         </Button>
       ) : (
         <Button onClick={handleClick} variant="contained">
@@ -75,20 +143,22 @@ export default function App() {
         </Button>
       )}
 
-      <ul>
-        {item.map((item) => {
-          console.log(item);
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)',columnGap: 3,
+    rowGap: 2, }}>
+        {item?.map((item) => {
           return (
             <Todolist
-              key={item.id}
-              id={item.id}
-              text={item.value}
+              key={item.todo_id}
+              id={item.todo_id}
+              task={item.task}
+              title={item.title}
+              time={item.time}
               onChecked={deleteItem}
               onEdit={editItem}
             />
           );
         })}
-      </ul>
+      </Box>
     </div>
   );
 }
--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -5,15 +5,52 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
 
-export default function Todolist(props) {
-  return (
-   <div>
-       <li>{props.text}<Button variant='text' onClick={()=>{
-         props.onEdit(props.id)
-       }}><EditIcon/></Button><Button variant="text" onClick={()=>{
-           props.onChecked(props.id)
-       }}><DeleteIcon/></Button></li>
 
-   </div>
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import Box from '@mui/material/Box';
+
+
+
+
+export default function Todolist(props) {
+  const [editClicked, setEditClicked] = React.useState(false);
+  return (
+  <Card sx={{ width: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {props.title?props.title[0]:"A"}
+          </Avatar>
+        }
+        
+        title={props.title}
+        subheader={props.time}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.task}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="edit" onClick={()=>{setEditClicked(true)}}>
+          <EditIcon onClick={()=>{
+      props.onEdit(props.id)}}></EditIcon>
+        </IconButton>
+        <IconButton aria-label="delete">
+          <DeleteIcon onClick={()=>{
+           props.onChecked(props.id)}}></DeleteIcon>
+        </IconButton>
+      </CardActions>
+      
+    </Card>
+    
   )
+
 }
